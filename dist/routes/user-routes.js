9a18c17ad7e3d6b635408ee84d91a4243eb8ac24
rnegic <arnego666@gmail.com>
2024-07-17 14:01:51 +0300

    Validation added

--- dist/routes/user-routes.js
+++ dist/routes/user-routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import { getAllUsers, userSignup } from "../controllers/user-controller.js";
+import { validate, signupValidator } from '../utils/validators.js';
 const userRoutes = Router();
 userRoutes.get('/', getAllUsers);
-userRoutes.post('/signup', userSignup);
+userRoutes.post('/signup', validate(signupValidator), userSignup);
 export default userRoutes;
 //# sourceMappingURL=user-routes.js.map
--- src/routes/user-routes.ts
+++ src/routes/user-routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { getAllUsers, userSignup } from "../controllers/user-controller.js";
+import { validate, signupValidator } from '../utils/validators.js';
 
 const userRoutes = Router();
 
 userRoutes.get('/', getAllUsers);
-userRoutes.post('/signup', userSignup);
+userRoutes.post('/signup', validate(signupValidator), userSignup);
 
 export default userRoutes;
