7745cf6eeca088e32ad5d785688cc53f00473c7f
rnegic <arnego666@gmail.com>
2024-07-28 19:40:22 +0300

    Sign in logic fixed/Sign out and sign up logic added

--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,15 @@ import { config } from "dotenv";
 import morgan from 'morgan';
 import appRouter from './routes/index.js';
 import cookieParser from "cookie-parser";
+import cors from 'cors';
 
 config();
 
 const app = express();
 
+app.use(cors({origin: "http://localhost:3000", credentials: true}))
 app.use(express.json());
-app.use(cookieParser(process.env.JWT_SECRET));
+app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(morgan("dev"));
 app.use("/api/v1", appRouter);
 
