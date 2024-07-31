79ef5bee00df7ea7bac9aaeb7307f12cd36af0b9
rnegic <arnego666@gmail.com>
2024-07-31 19:17:58 +0300

    Chat logic finished

--- src/routes/chat-routes.ts
+++ src/routes/chat-routes.ts
@@ -1,9 +1,16 @@
 import { Router } from "express";
 import { verifyToken } from "../utils/token-manager.js";
 import { chatCompletionValidator, validate } from "../utils/validators.js";
-import { generateChatCompletion } from "../controllers/chat-controller.js";
+import { deleteChats, generateChatCompletion, sendChatsToUser } from "../controllers/chat-controller.js";
 
 const chatRoutes = Router();
-chatRoutes.post("/new", validate(chatCompletionValidator), verifyToken, generateChatCompletion);
+chatRoutes.post(
+    "/new",
+    validate(chatCompletionValidator),
+    verifyToken,
+    generateChatCompletion
+);
+chatRoutes.get("/all-chats", verifyToken, sendChatsToUser);
+chatRoutes.delete("/delete", verifyToken, deleteChats);
 
 export default chatRoutes;
